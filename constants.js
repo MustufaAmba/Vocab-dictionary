ef0721bc8968ed6d524b6458170a8b192525fa14
MustufaAmba <79704556+MustufaAmba@users.noreply.github.com>
2025-12-09 17:21:22 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,1 @@
-const PORT = process.env.PORT || 5000;
-const app_id = "87ff13e9";
-const app_key="c3351aec319896459caafcb0de5554ef";
-const Database_key = "mongodb+srv://mustafa:Ma9924892469@cluster0.g26xc.mongodb.net/MERN?retryWrites=true&w=majority"
-module.exports={PORT,app_id,app_key,Database_key};
+
